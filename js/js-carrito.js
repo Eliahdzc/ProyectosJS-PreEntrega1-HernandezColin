db0d56ad6bcc6200363648e5899f136042970b21
Eliahdzc <eliamiriamhernandezcolin@gmail.com>
2023-04-26 16:10:15 -0600

    Cambios para tercera entrega

---
+++ js/js-carrito.js
@@ -0,0 +1,86 @@
+
+
+const nombreCompra = document.querySelector("#logo");
+
+function compraNombre() {
+    let objetoClienteCompra = JSON.parse(localStorage.getItem("cliente"))
+    const nombre = objetoClienteCompra.nombre
+    nombreCompra.innerText = `Gracias por tu compra, ${nombre} `
+}
+
+compraNombre()
+
+const carritoCompra = JSON.parse(localStorage.getItem("carrito"));
+console.log(carritoCompra);
+
+const contenedorCarritoVacio = document.querySelector("#carrito-vacio")
+const contenedorCarritoProductos = document.querySelector("#carrito-productos")
+const contenedorCarritoAcciones = document.querySelector("#carrito-acciones")
+const contenedorCarritoComprado = document.querySelector("#carrito-comprado")
+const totalCompra = document.querySelector("#carrito-acciones-total")
+const botonCompra = document.querySelector("#comprar")
+
+function cargarProductosCarrito() {
+if (carritoCompra) {
+
+    contenedorCarritoVacio.classList.add("disabled");
+    contenedorCarritoProductos.classList.remove("disabled");
+    contenedorCarritoAcciones.classList.remove("disabled");
+    contenedorCarritoComprado.classList.add("disabled");
+
+    contenedorCarritoProductos.innerHTML = "";
+
+    carritoCompra.forEach(producto => {
+
+        const div = document.createElement("div");
+        div.classList.add("carrito-producto");
+        div.innerHTML = `
+            <img class="carrito-producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+            <div class="carrito-producto-titulo">
+                <small>Titulo</small>
+                <h3>${producto.titulo}</h3>
+            </div>
+             <div class="carrito-producto-cantidad">
+                <small>Cantidad</small>
+                <p>${producto.cantidad}</p>
+             </div>
+            <div class="carrito-producto-precio">
+                <small>Precio</small>
+                <p>${producto.precio}</p>
+            </div>
+            <div class="carrito-producto-subtotal">
+                <small>Subtotal</small>
+                <p>${producto.precio * producto.cantidad}</p>
+            </div>
+            <button class="carrito-producto-eliminar" id="${producto.id}"><i class="bi bi-trash3"></i></button>
+        `;
+
+        contenedorCarritoProductos.append(div);
+    })
+
+} else {
+
+    contenedorCarritoVacio.classList.remove("disabled");
+    contenedorCarritoProductos.classList.add("disabled");
+    contenedorCarritoAcciones.classList.add("disabled");
+    contenedorCarritoComprado.classList.add("disabled");
+}
+}
+cargarProductosCarrito()
+
+
+function total(){
+    let compra = carritoCompra.reduce((acc, producto)=> acc + producto.precio, 0);
+    const totalNuevo = document.getElementById("total");
+    totalNuevo.innerText = `$ ${compra}`
+    console.log(compra)
+}
+total()
+
+botonCompra.addEventListener("click", compra)
+
+function compra() {
+    localStorage.setItem("carrito", JSON.stringify([]))
+    localStorage.removeItem("carrito")
+    location.href = "index.html"
+}
---
+++ js/js-registro.js
@@ -0,0 +1,60 @@
+
+const registro = document.querySelector("#registro")
+const nombre = document.querySelector("#nombre")
+const edad = document.querySelector("#edad")
+const tipoPiel = document.querySelector("#tipo-piel")
+const claveInicio = document.querySelector("#clave-usuario")
+const botonGuardar = document.querySelector("#boton-guardar")
+const checkboxPiSeca = document.querySelector("#piel-seca")
+const checkboxPiMixta = document.querySelector("#piel-mixta")
+const checkboxPiGrasa = document.querySelector("#piel-grasa")
+
+// const clientes = []
+
+botonGuardar.addEventListener("click", agregarNuevoCliente)
+
+
+function agregarNuevoCliente () {
+   const cliente = {
+    nombre: nombre.value,
+    edad: edad.value,
+    tipoPiel: {
+        pielSeca:checkboxPiSeca.checked,
+        pielMixta: checkboxPiMixta.checked,
+        pielGrasa: checkboxPiGrasa.checked,
+    },
+    clave: claveInicio.value
+   }
+   let clientes
+   console.log("cliente", cliente)
+   console.log(!localStorage.getItem("clientes"))
+   if(!localStorage.getItem("clientes")){
+       clientes = []
+} else {
+
+         clientes = JSON.parse(localStorage.getItem("clientes"))
+    }
+    clientes.push(cliente)
+    localStorage.setItem("clientes", JSON.stringify(clientes))
+    localStorage.setItem("cliente", JSON.stringify(cliente))
+    console.log(cliente.nombre)
+    // location.href = "recomendaciones.html"
+}
+
+let objetoCliente = JSON.parse(localStorage.getItem("cliente"))
+console.log(objetoCliente.nombre)
+
+
+
+
+
+// function nuevoCliente (nombr, eda, pie, clav) {
+    //     this.nombr = nombre,
+    //     this.eda = edad,
+    //     this.pie = tipoPiel,
+    //     this.clav = claveInicio
+    // }
+    
+    
+
+
